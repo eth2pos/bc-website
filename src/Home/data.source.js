5dd1efd616faf81284c52edbf7caee5d81aeab09
eth2pos <eth2pos@gmail.com>
2021-04-03 18:44:23 +0800

    init commit

---
+++ src/Home/data.source.js
@@ -0,0 +1,321 @@
+import React from 'react';
+export const Nav00DataSource = {
+  wrapper: { className: 'header0 home-page-wrapper' },
+  page: { className: 'home-page' },
+  logo: {
+    className: 'header0-logo kn0gnlzzso-editor_css',
+    children: 'https://i.imgur.com/khDzPD0.png',
+  },
+  Menu: {
+    className: 'header0-menu',
+    children: [
+      {
+        name: 'item~kn0j08ciens',
+        className: 'header0-item kn0j0c37cfe-editor_css',
+        children: {
+          href: '#',
+          children: [{ children: '导航四', name: 'text' }],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header0-mobile-menu' },
+};
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title kn0ixwb3q1u-editor_css',
+    children: 'https://i.imgur.com/fvpkpNJ.png',
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <span>
+          <span>
+            <p>BCoin，一个新型通缩资产，100层通兑塔经济模型</p>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  button: {
+    className: 'banner0-button kn0j0xjyco-editor_css',
+    children: 'Learn More',
+  },
+};
+export const Content00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>BC量子生态</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>100层通兑塔的模式</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <span>
+                        <p>
+                          作为一个通缩资产，BC的数量不断减少而价值不断升高!&nbsp;
+                        </p>
+                      </span>
+                    </span>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>持有BC免费产出DC</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>DC的价格会随着拆塔和销毁而不断上升</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: (
+                <span>
+                  <p>BC是量子空间母币</p>
+                </span>
+              ),
+            },
+            {
+              name: 'content',
+              children: (
+                <span>
+                  <p>
+                    BC作为量子生态空间的创世币，将会以不断通缩和上涨的态势为量子生态空间提供良好的生态环境
+                  </p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Feature40DataSource = {
+  wrapper: { className: 'home-page-wrapper content6-wrapper' },
+  OverPack: { className: 'home-page content6' },
+  textWrapper: {
+    className: 'content6-text kn1l5ta53y-editor_css',
+    xs: 24,
+    md: 10,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <span>
+              <p>通兑塔经济模型</p>
+            </span>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: '基于阿里云计算强大的基础资源',
+      },
+    ],
+  },
+  img: {
+    children: 'https://i.imgur.com/vtM99Dw.png',
+    className: 'content6-img kn1jx56bh5-editor_css',
+    xs: 24,
+    md: 14,
+  },
+  block: {
+    children: [
+      {
+        name: 'block0',
+        img: {
+          children:
+            'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          className: 'content6-icon',
+        },
+        title: {
+          className: 'content6-title',
+          children: (
+            <span>
+              <p>BCoin详情</p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content',
+          children: (
+            <span>
+              <p>最终发行量：4.8亿枚</p>
+              <p>最小单位：18位</p>
+              <p>发行价格：0.5$/枚（当前价格⬆️0.76$/枚）</p>
+              <p>最终目标价位：110$/枚</p>
+              <p>当前总市值/目标总市值：2.8亿美元/528亿美元</p>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block1',
+        img: {
+          className: 'content6-icon',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+        },
+        title: {
+          className: 'content6-title',
+          children: (
+            <span>
+              <span>
+                <p>通兑塔</p>
+              </span>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content',
+          children: (
+            <span>
+              <span>
+                <p>
+                  100层通兑塔的模式，当下通兑塔合约已经拆分到第 32
+                  层，已经从最初的0.5美元的价格上升到0.76美元
+                </p>
+              </span>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block2',
+        img: {
+          className: 'content6-icon',
+          children:
+            'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
+        },
+        title: {
+          className: 'content6-title',
+          children: (
+            <span>
+              <p>挖矿获取DC</p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content',
+          children: (
+            <span>
+              <p>
+                持有BC可以免费的产出更高价值的DC，DC的价格会随着拆塔和销毁而不断上升
+              </p>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <p>
+          <span>©2018 BCoin All Rights Reserved</span>
+        </p>
+      </span>
+    ),
+  },
+};
